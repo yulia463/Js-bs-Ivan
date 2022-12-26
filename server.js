b96c37d7689963550be4da429df9c36d2d59331e
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-12-26 16:03:03 +0100

    add server elements

---
+++ server.js
@@ -0,0 +1,6 @@
+const inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+    request.open();
+})
